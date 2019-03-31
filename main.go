dfc121b8543ba42ca40efbff61ac38a18e169ffc
NENG-DEV\NENG-DEV <suchat062.rungchaeng@gmail.com>
2019-03-31 14:31:10 +0700

    Add context

--- main.go
+++ main.go
@@ -9,16 +9,34 @@ import (
 )
 
 func main() {
+	var start, end string
+	start = "Command Line Interface"
+	end = "Command Line Interface"
+
 	app := cli.NewApp()
+	app.Name = "Command Line Claim"
+	app.Usage = "By Allianz.com (NENG)"
+	app.UsageText = "Date Format Parameter (YYYY-MM-DD) => -start=0000-00-00 -end=0000-00-00"
+	app.Version = "0.0.1"
+	app.Compiled = time.Now()
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "suchat rungchaeng",
+			Email: "suchat.ru@allianz.com",
+		},
+	}
+	app.Copyright = "(c) 2019 allianz.com"
 
 	app.Flags = []cli.Flag {
 		cli.StringFlag{
-			Name: "start",
-			Usage: "Start Date",
+			Name: "start, s",
+			Usage: "Start Date Format Parameter (YYYY-MM-DD) => -start=0000-00-00",
+			Destination: &start,
 		},
 		cli.StringFlag{
-			Name: "end",
-			Usage: "End Date",
+			Name: "end, e",
+			Usage: "End Date Format Parameter (YYYY-MM-DD) => -end=0000-00-00",
+			Destination: &end,
 		},
 	}
 
@@ -29,14 +47,16 @@ func main() {
 		End := c.String("end")
 		tStart, err := time.Parse("2006-01-02", Start)
 		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
+			//fmt.Println(err.Error())
+			return cli.NewExitError("Error => Start Date not format, -start=0000-00-00", 86)
+			//os.Exit(1)
 		}
 
 		tEnd, err := time.Parse("2006-01-02", End)
 		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
+			//fmt.Println(err.Error())
+			return cli.NewExitError("Error => End Date not format, -end=0000-00-00", 86)
+			//os.Exit(1)
 		}
 
 		if tStart.Unix() <= tEnd.Unix() {
